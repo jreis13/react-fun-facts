a1c5388e1fa9d620a947f3326b230d0f4853736a
Joao Reis <joaofranciscoreis13@outlook.com>
2022-07-21 18:19:50 +0100

    wrapping up project

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,3 +1,16 @@
+import styles from "./index.module.css";
+
 export default function Main() {
-  return <h1>Main goes here</h1>;
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Fun facts about React</h1>
+      <ul className={styles.list}>
+        <li>Was first released in 2013.</li>
+        <li>Was originally created by Jordan Walke.</li>
+        <li>Has well over 100K stars on Github.</li>
+        <li>Is maintained by Facebook.</li>
+        <li>Powers thousands of enterprise apps, including mobile apps.</li>
+      </ul>
+    </main>
+  );
 }
